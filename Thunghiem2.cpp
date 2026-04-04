0136f889b34656ba5ad1f0878ea1cb24a6912624
ĐẶNG VĂN DUY <Duydang792006@gmail.com>
2026-04-04 23:19:55 +0700

    Them vai sinh vien

--- Thunghiem2.cpp
+++ Thunghiem2.cpp
@@ -17,11 +17,28 @@ int main() {
     sv1.maSo = "202XXXXX"; // Thay mã số của bạn vào đây nhé
     sv1.diemGPA = 3.6;
 
+    SinhVien sv2;
+    sv2.hoTen = "Nguyen Hoang Son";
+    sv2.maSo = "202XXXXX"; // Thay mã số của bạn vào đây nhé
+    sv2.diemGPA = 3.6;
+
+    SinhVien sv3;
+    sv3.hoTen = "Doan Ba Quy";
+    sv3.maSo = "202XXXXX"; // Thay mã số của bạn vào đây nhé
+    sv3.diemGPA = 3.6;
+
     // 2. In thông tin ra màn hình
     cout << "--- Thong tin sinh vien ---" << endl;
     cout << "Ho ten: " << sv1.hoTen << endl;
     cout << "MSSV: " << sv1.maSo << endl;
     cout << "Diem GPA: " << sv1.diemGPA << endl;
-
+  cout << "--- Thong tin sinh vien ---" << endl;
+    cout << "Ho ten: " << sv2.hoTen << endl;
+    cout << "MSSV: " << sv2.maSo << endl;
+    cout << "Diem GPA: " << sv2.diemGPA << endl;
+      cout << "--- Thong tin sinh vien ---" << endl;
+    cout << "Ho ten: " << sv3.hoTen << endl;
+    cout << "MSSV: " << sv3.maSo << endl;
+    cout << "Diem GPA: " << sv3.diemGPA << endl;
     return 0;
 }
